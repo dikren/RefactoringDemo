44047b541b0f0a60538e47bb5618935b22936beb
dkhusnutdinov <dkhusnutdinov@ATC-10445.at-consulting.ru>
2015-09-22 16:24:32 +0300

    Many unit tests, stubs mostly

--- src/test/java/com/scrumtrek/simplestore/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -17,51 +17,81 @@ public class CustomerTest {
     @Before
     public void setUp() {
         String name = "Name";
-        title = "title Ours";
+        title = "Testing title";
          sut = new Customer(name);
          stubMovie = mock(Movie.class);
          stubRental = mock(Rental.class);
     }
     
-    @Test
-    public void shouldReturnRightStringWhenRentalIsNewRelease() {
-        when(stubMovie.getTitle()).thenReturn("Teting title");
-        when(stubMovie.getPriceCode()).thenReturn(PriceCodes.NewRelease);
+    private void setUpWithParams(PriceCodes pc, int daysRented) {
+        when(stubMovie.getTitle()).thenReturn("Testing title");
+        when(stubMovie.getPriceCode()).thenReturn(pc);
         
         when(stubRental.getMovie()).thenReturn(stubMovie);
-        when(stubRental.getDaysRented()).thenReturn(9);
+        when(stubRental.getDaysRented()).thenReturn(daysRented);
+    }
+    
+    @Test
+    public void shouldReturnRightStringWhenRentalIsNewRelease() {
+        setUpWithParams(PriceCodes.NewRelease, 9);
+        
+        sut.addRental(stubRental);
+        
+        assertEquals("Rental record for Name\n\tTesting title\t27.0\nAmount owed is 27.0\nYou earned 2 frequent renter points.", sut.Statement());
+    }
+    
+    @Test
+    public void shouldReturnRightStringWhenRentalIsRegularAndDaysRentedBiggerThan2() {
+        setUpWithParams(PriceCodes.Regular, 9);
         
         
         sut.addRental(stubRental);
         
-        assertEquals("Rental record for Name\n\tTeting title\t27.0\nAmount owed is 27.0\nYou earned 2 frequent renter points.", sut.Statement());
+        assertEquals("Rental record for Name\n\tTesting title\t12.5\nAmount owed is 12.5\nYou earned 1 frequent renter points.", sut.Statement());
     }
     
     @Test
-    public void shouldTestRentalWhenTestingIt12() {
-        
-        Movie m = new Movie(title, PriceCodes.Regular);
-        
-        Rental rent = new Rental(m,  9); 
+    public void shouldReturnRightStringWhenRentalIsRegularAndDaysRentedLessThan2() {
+        setUpWithParams(PriceCodes.Regular, 1);
         
         
-        sut.addRental(rent);
+        sut.addRental(stubRental);
         
-        assertEquals("Rental record for Name\n\ttitle Ours\t12.5\nAmount owed is 12.5\nYou earned 1 frequent renter points.", sut.Statement());
+        assertEquals("Rental record for Name\n\tTesting title\t2.0\nAmount owed is 2.0\nYou earned 1 frequent renter points.", sut.Statement());
     }
     
     @Test
-    public void shouldTestRentalWhenTestingIfgdt() {
+    public void shouldReturnRightStringWhenRentalIsChildrensAndDaysRentedLessThan3() {
+        setUpWithParams(PriceCodes.Childrens, 1);
+
+        sut.addRental(stubRental);
         
-        Movie m = new Movie(title, PriceCodes.Childrens);
-        
-        Rental rent = new Rental(m,  9); 
-        
-        
-        sut.addRental(rent);
-        
-        assertEquals("Rental record for Name\n\ttitle Ours\t9.0\nAmount owed is 9.0\nYou earned 1 frequent renter points.",
+        assertEquals("Rental record for Name\n\tTesting title\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points.",
                 sut.Statement());
+    }
+    
+
+    @Test
+    public void shouldReturnRightStringWhenRentalIsChildrensAndDaysRentedBiggerThan3() {
+        setUpWithParams(PriceCodes.Childrens, 9);
+        
+        sut.addRental(stubRental);
+        
+        assertEquals("Rental record for Name\n\tTesting title\t9.0\nAmount owed is 9.0\nYou earned 1 frequent renter points.",
+                sut.Statement());
+    }
+    
+    @Test
+    public void shouldCallGetDaysRentedAtLeastOneTimeWhenCallStatement() {
+        setUpWithParams(PriceCodes.Childrens, 9);
+        
+        sut.addRental(stubRental);
+        
+        sut.Statement();
+        
+        verify(stubRental, atLeastOnce()).getDaysRented();
+        
+        
     }
 
 }
