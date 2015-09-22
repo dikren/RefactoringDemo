6d13e58a356b31f35c73e92619015e35cf200a3d
dkhusnutdinov <dkhusnutdinov@ATC-10445.at-consulting.ru>
2015-09-22 16:00:27 +0300

    commmit real unit test

--- src/test/java/com/scrumtrek/simplestore/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -1,33 +1,40 @@
 package com.scrumtrek.simplestore;
 
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class CustomerTest {
     
     private String title ;
     private Customer sut;
+    private Movie stubMovie;
+    private Rental stubRental;
     
     @Before
     public void setUp() {
         String name = "Name";
         title = "title Ours";
          sut = new Customer(name);
-        
+         stubMovie = mock(Movie.class);
+         stubRental = mock(Rental.class);
     }
     
     @Test
-    public void shouldTestRentalWhenTestingIt() {
+    public void shouldReturnRightStringWhenRentalIsNewRelease() {
+        when(stubMovie.getTitle()).thenReturn("Teting title");
+        when(stubMovie.getPriceCode()).thenReturn(PriceCodes.NewRelease);
         
-        Movie m = new Movie(title, PriceCodes.NewRelease);
+        when(stubRental.getMovie()).thenReturn(stubMovie);
+        when(stubRental.getDaysRented()).thenReturn(9);
         
-        Rental rent = new Rental(m,  9); 
         
+        sut.addRental(stubRental);
         
-        sut.addRental(rent);
-        
-        assertEquals("Rental record for Name\n\ttitle Ours\t27.0\nAmount owed is 27.0\nYou earned 2 frequent renter points.", sut.Statement());
+        assertEquals("Rental record for Name\n\tTeting title\t27.0\nAmount owed is 27.0\nYou earned 2 frequent renter points.", sut.Statement());
     }
     
     @Test
